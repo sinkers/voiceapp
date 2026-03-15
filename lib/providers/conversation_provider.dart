a57fa27fdb59a5d6b49367710082cfef8a235f4d
Andrew Sinclair <ajsinclair@gmail.com>
2026-03-15 17:18:07 +1100

    Fix dart format on conversation_provider.dart

--- lib/providers/conversation_provider.dart
+++ lib/providers/conversation_provider.dart
@@ -294,10 +294,9 @@ class ConversationProvider extends ChangeNotifier {
             rate: _settings.ttsRate, pitch: _settings.ttsPitch);
         _ttsService = svc;
       case TtsProvider.elevenlabs:
-        final voiceId =
-            (_settings.elevenLabsVoiceId.isNotEmpty)
-                ? _settings.elevenLabsVoiceId
-                : _settings.elevenLabsVoice?.voiceId ?? '21m00Tcm4TlvDq8ikWAM';
+        final voiceId = (_settings.elevenLabsVoiceId.isNotEmpty)
+            ? _settings.elevenLabsVoiceId
+            : _settings.elevenLabsVoice?.voiceId ?? '21m00Tcm4TlvDq8ikWAM';
         final svc = ElevenLabsTtsService(
           apiKey: _settings.elevenLabsApiKey ?? '',
           voiceId: voiceId,
